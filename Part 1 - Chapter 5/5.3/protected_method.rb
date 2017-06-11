91b6f1172041b889edaeb839c4b6b90209b0241b
Tiago Bioucas <tiago.bioucas@eae.pt>
2017-06-11 18:53:52 +0100

    Chapter 5 -> 5.3

---
+++ Part 1 - Chapter 5/5.3/protected_method.rb
@@ -0,0 +1,24 @@
+class C
+  def initialize (n)
+    @n = n
+  end
+
+  def n
+    @n
+  end
+
+  def compare (c)
+    if c.n > n
+      puts "Bigger"
+    else
+      puts "Smaller"
+    end
+  end
+
+  protected :n
+end
+
+c1 = C.new(100)
+c2 = C.new(200)
+
+c1.compare(c2)
