f171ba541e28c540e093ef4bb83a9351e9e6954a
Tiago Bioucas <tiago.bioucas@eae.pt>
2017-06-15 22:09:17 +0100

    Chapter 6 -> 6.3

---
+++ Part 1 - Chapter 6/6.3/map.rb
@@ -0,0 +1,4 @@
+names = ["David", "James", "William"]
+puts names
+names = names.map { |name| name.upcase }
+puts names
---
+++ Part 1 - Chapter 6/6.3/times.rb
@@ -0,0 +1,4 @@
+5.times { puts "Phrase"}
+
+
+5.times { |i| puts "Counting #{i}"}
