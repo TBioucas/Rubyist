81b6a1177f3122b8a49a6bbfbd044ef4826beeed
Tiago Bioucas <tiago.bioucas@eae.pt>
2017-06-28 12:34:58 +0100

    Chapter 8 -> 8.2

---
+++ Part 2 - Chapter 8/8.2/hash.rb
@@ -0,0 +1,11 @@
+d_hash = {:name => 'David', :age => 20}
+
+puts d_hash[:name]
+
+t_hash = {"name" => 'Thomas', "age" => 50}
+
+puts t_hash["name"]
+
+a_hash = {name: 'Anthony', age: 30}
+
+puts a_hash[:name]
---
+++ Part 2 - Chapter 8/8.2/symbols.rb
@@ -0,0 +1,3 @@
+puts "a".to_sym
+puts "a".intern
+puts :a.to_s
