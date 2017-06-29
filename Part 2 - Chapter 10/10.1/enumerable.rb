3578922fbc9539675772d187c8fc23ebf4c73afe
Tiago Bioucas <tiago.bioucas@eae.pt>
2017-06-29 16:43:40 +0100

    Chapter 10 -> 10.1

---
+++ Part 2 - Chapter 10/10.1/enumerable.rb
@@ -0,0 +1,23 @@
+class Rainbow
+  include Enumerable
+
+  def each
+    yield "red"
+    yield "orange"
+    yield "yellow"
+    yield "green"
+    yield "blue"
+    yield "indigo"
+    yield "violet"
+  end
+end
+
+r = Rainbow.new
+
+r.each do |color|
+  puts "Next color: #{color}"
+end
+
+y_color = r.find {|color| color.start_with?("y")}
+
+puts "Colors starting with Y: #{y_color}"
